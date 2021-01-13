76ee9bf4a87c309f8a6b2116bdb2851843715a08
eduCarv <edu.ascarvalho@gmail.com>
2021-01-12 23:50:59 -0300

    Corrige start do servidor

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -10,7 +10,7 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
         let errors: ValidationErrors = {};
 
         error.inner.forEach(err => {
-            errors[err.path] = err.errors;
+            errors[err.name] = err.errors;
         });
 
         return response.status(400).json({ message: 'Validadion fails', errors });
